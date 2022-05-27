51f892d21054d2b579a1a281a751a3240441a499
tomba333 <74898579+tomba333@users.noreply.github.com>
2022-05-27 14:52:45 +0200

    Add files via upload

--- Projekt_Web/insert.php
+++ Projekt_Web/insert.php
@@ -8,13 +8,17 @@ $prezime = $_REQUEST['prezime'];
 $datum = $_REQUEST['datum'];
 $email = $_REQUEST['email'];
 $br_gostiju = $_REQUEST['br_gostiju'];
-$sql = "INSERT INTO rezervacija (ime,prezime,datum,email,broj_gostiju)
-VALUES ('$ime','$prezime','$datum','$email','$br_gostiju')";
+$stol =isset($_POST['stol'])?$_POST['stol']:'not yet';
+$sql = "INSERT INTO rezervacija (ime,prezime,datum,email,broj_gostiju,stol)
+VALUES ('$ime','$prezime','$datum','$email','$br_gostiju','$stol')";
 if (mysqli_query($conn, $sql)) {
     header('Location: ./index.php');
 } else {
     echo "Error: " . $sql . ":-" . mysqli_error($conn);
 }
+
+
+
 CloseCon($conn);
 
 
